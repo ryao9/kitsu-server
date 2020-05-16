90f17dcbd5aacfa566a676c7499d52c85cccd99b
toyhammered <daniel@rassiner.com>
2020-05-16 13:14:38 -0700

    refactor category connection and edge

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -1,2 +1,9 @@
 class Types::BaseConnection < GraphQL::Types::Relay::BaseConnection
+  field :total_count, Integer,
+    null: false,
+    description: 'The total amount of children for this category'
+
+  def total_count
+    object.nodes.count
+  end
 end
