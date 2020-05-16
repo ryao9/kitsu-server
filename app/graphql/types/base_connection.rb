d3b1c78961fe22c04714928178ba1182fc97489e
toyhammered <daniel@rassiner.com>
2020-05-16 13:18:03 -0700

    update description for total_count
    still unsure if that is a great description :shrug:

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -1,7 +1,7 @@
 class Types::BaseConnection < GraphQL::Types::Relay::BaseConnection
   field :total_count, Integer,
     null: false,
-    description: 'The total amount of children for this category'
+    description: 'The total amount of nodes.'
 
   def total_count
     object.nodes.count
